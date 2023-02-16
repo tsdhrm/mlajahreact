c8063130327ffd05739db9853e1c273b149220ee
aphrobite <aphrobite@pop-os.localdomain>
2023-02-16 14:13:28 +0800

    new components

--- src/App.jsx
+++ src/App.jsx
@@ -23,28 +23,44 @@ function App() {
   return (
     <div>
       <h1>My Hacker Stories</h1>
-
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <Search />
 
       <hr />
 
-      <ul>
-        {list.map(function (item) {
-          return (
-            <li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </li>
-          );
-        })}
-      </ul>
+      <List />
     </div>
   );
+
+  function Search() {
+    return (
+      <div>
+        <label htmlFor="search">Search: </label>
+        <input id="search" type="text" />
+      </div>
+    )
+  };
+
+  function List() {
+    return (
+      <div>
+        <ul>
+          {list.map(function (item) {
+            return (
+              <li key={item.objectID}>
+                <span>
+                  <a href={item.url}>{item.title}</a>
+                </span>
+                <span>{item.author}</span>
+                <span>{item.num_comments}</span>
+                <span>{item.points}</span>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+    )
+  }
 }
 
 export default App;
